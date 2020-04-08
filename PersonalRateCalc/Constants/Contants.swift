d9748da0a6a0ffc87de5eb00ff3e2aaf426e1ba5
Dospayev Tair <tair.dospayev@gmail.com>
2020-04-08 23:45:12 +0600

    add constants

---
+++ PersonalRateCalc/Constants/Contants.swift
@@ -0,0 +1,30 @@
+//
+//  Contants.swift
+//  PersonalRateCalc
+//
+//  Created by Tair Dospayev on 4/8/20.
+//  Copyright © 2020 Tair Dospayev. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+  static let mainBackgroundColor = UIColor(red: 0.98, green: 0.98, blue: 1.00, alpha: 1.00)
+  static let lightGrey = UIColor(red: 0.75, green: 0.76, blue: 0.79, alpha: 1.00)
+}
+
+struct Fonts {
+  static let rateContent = UIFont(name: "Abel", size: 22)
+  static let date = UIFont(name: "Abel", size: 30)
+}
+
+struct Rate {
+  static let navTitle = "Currency rate"
+  static let purchaseTitle = "Purchase"
+  static let saleTitle = "Sale"
+  static let currencyTitle = "Currency"
+}
+
+struct Calc {
+  static let navTitle = "Calculate"
+}
